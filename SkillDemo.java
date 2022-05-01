27067692e3293373e62a15dd65f3c0f8acd1ce34
brendan887 <mahbrendan@gmail.com>
2022-05-01 00:17:52 -0700

    Created multipleOfSeven() and tester file

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,8 @@
 public class SkillDemo{
-
+    public static boolean multipleOfSeven(int n) {
+        if (n % 7 == 0){
+            return true;
+        }
+        return false;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+    @Test
+    public void isMultipleOfSeven() {
+        assertTrue(SkillDemo.multipleOfSeven(13));
+    }
+}
